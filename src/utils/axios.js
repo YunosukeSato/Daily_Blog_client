e7062351b4bc75325c905b32037d0e2848ced2eb
Yunosuke Sato <yunosukesato04@gmail.com>
2023-09-25 16:43:31 -0700

    fixed baseURL to the server production URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "https://daily-blog-server.onrender.com";
 axios.defaults.withCredentials = true;
 
 export default axios;
